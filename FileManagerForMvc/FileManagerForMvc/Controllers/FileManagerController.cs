856a5d0e62bb985d6b892d8e1b09601fe1d22954
sunboy <parsdarab@gmail.com>
2017-05-04 03:47:45 +0430

    file

---
+++ FileManagerForMvc/FileManagerForMvc/Controllers/FileManagerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileManagerForMvc.Controllers
+{
+    public class FileManagerController : Controller
+    {
+        //
+        // GET: /FileManager/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
